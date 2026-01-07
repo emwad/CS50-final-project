167feabee42a150b240fddf35610aa32ea6e951c
Emily <ejwade@hotmail.co.uk>
2026-01-07 17:42:08 +0000

    not sure!

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from helpers import close_db, inst, get_db
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -10,14 +11,21 @@ app.teardown_appcontext(close_db)
 def index():
     # POST
     if request.method == "POST":
-        ukprn = int(request.form.get("inst1"))
+        ukprn1 = int(request.form.get("inst1"))
+        ukprn2 = int(request.form.get("inst2"))
         db = get_db()
 
         # Note - fetchone() brings back 1 row, fetchall() LIST of rows 
         # (so will need cycling through in template)
         # this bit just checking i can bring back the right info for now
-        rows = db.execute("SELECT * FROM institutions WHERE ukprn = ?", (ukprn,)).fetchone()
-        address = db.execute("SELECT * FROM addresses WHERE ukprn = ?", (ukprn,)).fetchone()
-        return render_template('test.html', inst1=rows, inst=address)
+        rows = db.execute("SELECT * FROM nss WHERE ukprn = ?", (ukprn1,)).fetchone()
+        rows2 = db.execute("SELECT * FROM nss WHERE ukprn = ?", (ukprn2,)).fetchone()
+        plt.barh([rows["PROVIDER_NAME"], rows2["PROVIDER_NAME"]], 
+                 [rows["POSITIVITY_MEASURE"], rows2["POSITIVITY_MEASURE"]])
+        plt.ylabel("Institution")
+        plt.xlabel("Positivity measure") 
+        plt.title("Horizontal bar graph")
+        chart = plt.show()
+        return render_template('comparison.html', inst1=rows, inst2=rows2, chart=chart)
 
     return render_template('index.html')
